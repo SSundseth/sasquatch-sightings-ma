559bcf39b21101058fea6571988d68b8d81f9a3d
Stefan Sundseth <stefan.sundseth@accenturefederal.com>
2019-12-18 11:31:36 -0500

    add gallery

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ function loadImg() {
 	mainImg.src = imgUrl;
 }
 
+function loadGallery() {
+	var container = document.getElementById("imgContainer");
+
+	var html = "";
+
+	for (var i=0; i <= NUM_FILES; i++) {
+		html += "<div class='col-lg-3 col-md-4 col-6'>";
+		html += "<a href='#' class='d-block mb-4 h-100'>";
+		html += "<img class='img-fluid img-thumbnail' src='images/"+i+".jpg' alt=''>";
+		html += "</a></div>";
+	}
+
+	container.innerHTML = html;
+
+
+}
